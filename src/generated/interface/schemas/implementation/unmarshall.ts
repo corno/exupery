40442060c3a801d5d058547b630801cea99d8990
Corno Schraverus <corno@schraverus.com>
2025-08-02 20:41:40 +0200

    latest generated

---
+++ src/generated/interface/schemas/implementation/unmarshall.ts
@@ -0,0 +1,271 @@
+import * as _pt from 'exupery-core-types'
+
+import * as _i_in from "./resolved"
+import * as _i_out from "./unresolved"
+
+// **** TYPES
+
+export type _T_Initialization = (
+    $$_: _i_in._T_Initialization,
+    $$_p: null,
+) => _i_out._T_Initialization<null>
+
+export type _T_Literal = (
+    $$_: _i_in._T_Literal,
+    $$_p: null,
+) => _i_out._T_Literal<null>
+
+export type _T_Module = (
+    $$_: _i_in._T_Module,
+    $$_p: null,
+) => _i_out._T_Module<null>
+
+export type _T_Module_Set = (
+    $$_: _i_in._T_Module_Set,
+    $$_p: null,
+) => _i_out._T_Module_Set<null>
+
+export type _T_Selection = (
+    $$_: _i_in._T_Selection,
+    $$_p: null,
+) => _i_out._T_Selection<null>
+
+export type _T_Type_Parameters = (
+    $$_: _i_in._T_Type_Parameters,
+    $$_p: null,
+) => _i_out._T_Type_Parameters<null>
+
+export type _T_Variables = (
+    $$_: _i_in._T_Variables,
+    $$_p: null,
+) => _i_out._T_Variables<null>
+
+// **** FRIENDLY NAMES FOR THE GLOBAL TYPES
+
+export type Initialization = _T_Initialization
+
+export type Literal = _T_Literal
+
+export type Module = _T_Module
+
+export type Module_Set = _T_Module_Set
+
+export type Selection = _T_Selection
+
+export type Type_Parameters = _T_Type_Parameters
+
+export type Variables = _T_Variables
+
+// **** ALIASES FOR NESTED TYPE WITH PREFIXED ROOT NAMES
+
+export namespace _T_Initialization {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Initialization
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Initialization<null>
+}
+
+export namespace _T_Literal {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Literal
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Literal<null>
+}
+
+export namespace _T_Module {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Module
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Module<null>
+}
+
+export namespace _T_Module_Set {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Module_Set
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Module_Set<null>
+}
+
+export namespace _T_Selection {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Selection
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Selection<null>
+}
+
+export namespace _T_Type_Parameters {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Type_Parameters
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Type_Parameters<null>
+}
+
+export namespace _T_Variables {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Variables
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Variables<null>
+}
+
+// *** ALIASES FOR NESTED TYPES
+
+export namespace Initialization {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Initialization
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Initialization<null>
+}
+
+export namespace Literal {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Literal
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Literal<null>
+}
+
+export namespace Module {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Module
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Module<null>
+}
+
+export namespace Module_Set {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Module_Set
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Module_Set<null>
+}
+
+export namespace Selection {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Selection
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Selection<null>
+}
+
+export namespace Type_Parameters {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Type_Parameters
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Type_Parameters<null>
+}
+
+export namespace Variables {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Variables
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+        export type annotation = null
+    }
+    export type RESULT = _i_out._T_Variables<null>
+}
